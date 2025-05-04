beeb044988647179c93415bf7834c2fa6600fabb
identityofsine <identityofsine@gmail.com>
2025-05-04 00:08:57 -0400

    fixed nodemon script

--- api/src/blockmen/__init__.py
+++ api/src/blockmen/__init__.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 logDebug("Hello World")
 logDebug("Hello World 2")
 logDebug("Hello World 3")
--- api/start_dev.sh
+++ api/start_dev.sh
@@ -1,2 +1,1 @@
-#nodemon --watch src/** --exec "python ./src/blockmen/__init__.py"
 nodemon src/blockmen/__init__.py
