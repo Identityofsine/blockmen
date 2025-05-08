c0f0a165da99d2e42f6ec4460a7f5f3c6ea56478
identityofsine <identityofsine@gmail.com>
2025-05-07 21:38:47 -0400

    added frametime

--- webapp/src/app/App.tsx
+++ webapp/src/app/App.tsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.scss'
 import { getBuildConfig } from '../util/build'
+import Canvas from './canvas/canvas';
+import Renderer from './renderer/renderer';
 
 
 function App() {
 
 	const buildInfo = getBuildConfig();
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const renderer = useRef<Renderer>(null);
+
+	useEffect(() => {
+		if (canvasRef.current) {
+			renderer.current = new Renderer(canvasRef.current, { dpi: 2 });
+		}
+	}, [canvasRef.current])
+
 
 	return (
 		<div>
 			<h1>Max Ward</h1>
 			<p>({buildInfo.branch}:{buildInfo.buildId})-{buildInfo.buildDate}</p>
+			<canvas id="canvas" ref={canvasRef} width={1024} height={1024} style={{ border: "1px solid black" }}></canvas>
 		</div>
 	)
 }
